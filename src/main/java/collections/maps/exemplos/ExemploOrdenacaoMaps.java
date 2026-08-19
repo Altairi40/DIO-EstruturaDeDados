b9ceb899a26ed585c7a9631776a7c28770838733
Altairi40 <lucas.shiraoka@gmail.com>
2026-08-19 12:58:07 -0300

    "Collections_ Maps - Exemplos: Adicionando '\n' para separação de linhas, melhorando a visualização"

--- src/main/java/collections/maps/exemplos/ExemploOrdenacaoMaps.java
+++ src/main/java/collections/maps/exemplos/ExemploOrdenacaoMaps.java
@@ -14,7 +14,7 @@ Autor = Harari, Yuval Noah  - Livro = 21 Lições Para o Século 21, páginas: 4
 public class ExemploOrdenacaoMaps {
     public static void main(String[] args) {
 
-        System.out.println("--\tOrdem aleatória\t--");
+        System.out.println("\n--\tOrdem aleatória\t--");
         Map<String, Livro> meusLivros = new HashMap<>() {{
             put(" Hawking, Stephen", new Livro("Uma Breve História do Tempo", 256));
             put(" Duhigg, Charles", new Livro("O Poder do Hábito", 408));
@@ -23,7 +23,7 @@ public class ExemploOrdenacaoMaps {
         for (Map.Entry<String, Livro> livro : meusLivros.entrySet())
             System.out.println(livro.getKey() + " - " + livro.getValue().getNome());
 
-        System.out.println("--\tOrdem Inserção\t--");
+        System.out.println("\n--\tOrdem Inserção\t--");
         Map<String, Livro> meusLivros1 = new LinkedHashMap<>() {{
             put(" Hawking, Stephen", new Livro("Uma Breve História do Tempo", 256));
             put(" Duhigg, Charles", new Livro("O Poder do Hábito", 408));
@@ -32,12 +32,12 @@ public class ExemploOrdenacaoMaps {
         for (Map.Entry<String, Livro> livro : meusLivros1.entrySet())
             System.out.println(livro.getKey() + " - " + livro.getValue().getNome());
 
-        System.out.println("--\tOrdem alfabética autores\t--");
+        System.out.println("\n--\tOrdem alfabética autores\t--");
         Map<String, Livro> meusLivros2 = new TreeMap<>(meusLivros1);
         for (Map.Entry<String, Livro> livro : meusLivros2.entrySet())
             System.out.println(livro.getKey() + " - " + livro.getValue().getNome());
 
-        System.out.println("--\tOrdem alfabética nomes dos livros\t--");
+        System.out.println("\n--\tOrdem alfabética nomes dos livros\t--");
 
         Set<Map.Entry<String, Livro>> meusLivros3 = new TreeSet<>(new ComparatorNome());
         meusLivros3.addAll(meusLivros.entrySet());
